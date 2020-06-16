b7350fa25724b23fd827692a2097eeddcfb1f97e
yuri0820 <66296409+yuri0820@users.noreply.github.com>
2020-06-16 11:12:57 +0900

    씬애니추가

---
+++ Assets/Scripts/FadeInAnim2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInAnim2 : MonoBehaviour
+{
+
+    float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 3f)
+        {
+            GetComponent<TextMesh>().color = new Color(1, 1, 1, time / 1);
+            time += Time.deltaTime;
+        }
+        else
+        {
+
+        }
+
+
+    }
+
+    public void resetAnim()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        this.gameObject.SetActive(true);
+        time = 0;
+    }
+}
